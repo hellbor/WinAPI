4ad1e38fb9b7ae70ae06c99478370ebdffbf5861
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-10-30 21:17:26 +0300

    Copy DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -32,8 +32,8 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CHAR sz_buffer[SIZE] = {};
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-			SendMessage(GetDlgItem(hwnd,IDC_BUTTON_COPY), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(GetDlgItem(hwnd,IDC_BUTTON_COPY), WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK!", "Info", MB_OK | MB_ICONINFORMATION); break;
