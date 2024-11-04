1ad08694d0cbcc0303db9ad253c26fc059a59285
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-11-04 17:14:00 +0300

    List Box in progress

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -3,13 +3,13 @@
 #include<stdio.h>
 #include"resource.h"
 
-CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "Combo", "Box" };
+CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, DlgProc, 0);
 	return 0;
 }
 
@@ -22,13 +22,12 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
 		for (int i = 0; i < sizeof(g_VALUES) / sizeof(g_VALUES[0]); i++)
 		{
-			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
-			//CB_ - ComboBox
+			SendMessage(hList, CB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
 		}
-		SendMessage(hCombo, CB_SETCURSEL, 0, 0);
+		//SendMessage(hList, CB_SETCURSEL, 0, 0);
 	}
 	break;
 	case WM_COMMAND:
@@ -37,19 +36,15 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE]{};	//sz_ - String Zero (NULL-TerminateLine)
-			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
-			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-			//GetCurrenntSelection - возвращает номер выделенного элемента ComboBox.
-			//Элементы нумеруются с нуля.
-
-			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
-			//Сохраяняет текст указанного элемента ComboBox в указанный буфер.
+			CHAR sz_buffer[SIZE]{};
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hList, CB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
 
 			CHAR sz_message[SIZE]{};
 			sprintf(sz_message, "Вы выбрали пункт №%i со значением\"%s\".", i, sz_buffer);
 
-			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			//MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
 		break;
 		case IDCANCEL:EndDialog(hwnd, 0); break;
