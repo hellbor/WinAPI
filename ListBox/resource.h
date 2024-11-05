9354d1a6196ef0fd7bc43f1686abc7f6b32eec48
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-11-05 21:42:20 +0300

    AddItem to ListBox DONE

--- ListBox/resource.h
+++ ListBox/resource.h
@@ -4,19 +4,20 @@
 //
 #define IDD_DIALOG                      101
 #define IDI_ICON1                       102
-#define IDC_LIST                        1001
+#define IDD_DIALOG_ADD                  104
 #define IDC_LIST1                       1001
-#define IDC_SCROLLBAR1                  1002
-#define IDC_BUTTON2                     1009
-#define IDC_BUTTON3                     1010
+#define IDC_BUTTON_ADD                  1009
+#define IDC_BUTTON_DELETE               1010
+#define IDC_EDIT1                       1011
+#define IDC_EDIT_ADD                    1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
